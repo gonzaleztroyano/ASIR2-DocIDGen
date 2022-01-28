22fc616662efde68724799de0f8c2e71cd2a6c1b
Pablo Gonzalez Troyano <pablogontroya@gmail.com>
2022-01-28 23:42:05 +0100

    Corrección 220128

--- index.php
+++ index.php
@@ -11,21 +11,22 @@ print_r($_POST);
     <body>
         <div id="header">
             <h1>Nomenclatura Elementos ASIR</h1>
-            <pre class="code-style"> <span>ASIR2.{MÓDULO}.T{#}.[#].{CONCEPTO}.PabloGonzález.{ext}</span></pre>
+            <pre class="code-style"> <span>ASIR2.{MÓDULO}.T{#}.[#].{TÍTULO}.PabloGonzález.{ext}</span></pre>
         </div>
         <div id="data-intro">
         <form action="index.php" method="post">
                 <label for="modulo" class="need-vertical-margin">Módulo: &nbsp;&nbsp;&nbsp;</label>
-                <select id="modulo" name="modulo" tabindex="1" class="need-vertical-margin>
+                <select id="modulo" name="modulo" tabindex="1">
                     <option value="ASGBD">ASGBD</option>
                     <option value="ASO">ASO</option>
                     <option value="IAW">IAW</option>
+                    <option value="ING">ING</option>
                     <option value="SRI">SRI</option>
                     <option value="SYAD">SYAD</option>
                 </select>
                 <fieldset class="need-vertical-margin">
                     <legend>Tema o Trimestre</legend>
-                    <label for="modulo">Módulo: &nbsp;&nbsp;&nbsp;</label>
+                    <label for="t_o_trim">Módulo: &nbsp;&nbsp;&nbsp;</label>
                     <select id="t_o_trim" name="t_o_trim" tabindex="2">
                         <option value="T">Tema</option>
                         <option value="Trim">Trimestre</option>
@@ -33,16 +34,19 @@ print_r($_POST);
                     <label for="numero">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Número: &nbsp;&nbsp;&nbsp;</label>
                     <input name="numero" id="numero" type="number" tabindex="3" min="0"/>    
                 </fieldset>
+                <fieldset>
+                <legend>Título y extensión</legend>
                 <label for="titulo" class="need-vertical-margin">Título: &nbsp;&nbsp;&nbsp;</label>
-                <input name="titulo" id="titulo" type="text" tabindex="4" class="need-vertical-margin"/>    
+                <input name="titulo" id="titulo" type="text" tabindex="4" />    
                     </br>
-                <input type="submit" name="enviar" value="Generar" id="submit-button">
+                </fieldset>
+                <input type="submit" name="enviar" value="Generar" id="submit-button" class="need-vertical-margin">
             </form>
     </div>
     <?php
         if(isset($_POST["enviar"])){  ?>
             <div id="gen-result">
-                <pre class="code-style"> <span>ASIR2.<?=$_POST["t_o_trim"]?><?$_POST["numero"]?>.[#].{CONCEPTO}.PabloGonzález.{ext}</span></pre>
+                <pre class="code-style"> <span>ASIR2.<?=$_POST["modulo"]?>.<?=$_POST["t_o_trim"]?><?=$_POST["numero"]?>.<?=$_POST["titulo"]?>.PabloGonzález.{ext}</span></pre>
             </div>
         <?php }
     ?>
